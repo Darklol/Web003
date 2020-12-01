bc03553d0b80c35963b2620cebcacdb91a988af4
hsy <975892929@qq.com>
2020-12-02 00:48:25 +0800

    Your ROLEX is coming!

--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -1,1 +1,34 @@
-// I want to be a clock! Please help me Sasha!
+const INTERVAL = 9000;
+
+function getFormatString(numb) {
+    return numb < 10 ? '0' + String(numb) : String(numb);
+}
+
+function setTime(hours, minutes, seconds){
+    const hoursInDegrees = (((hours%12)*3600+minutes*60+seconds)/(12*3600))*360;
+    const minutesInDegrees = ((minutes*60+seconds)/3600)*360;
+    const secondsInDegrees = seconds*6;
+    $("#seconds").attr({transform: `rotate(${secondsInDegrees} 150 150)`});
+    $("#minutes").attr({transform: `rotate(${minutesInDegrees} 150 150)`});
+    $("#hours").attr({transform: `rotate(${hoursInDegrees} 150 150)`});
+}
+
+function updateClock() {
+    const currentDate = new Date();
+
+    const year = currentDate.getFullYear();
+    const month = getFormatString(currentDate.getMonth()+1);
+    const day = getFormatString(currentDate.getDate());
+
+    const hours = currentDate.getHours();
+    const minutes = currentDate.getMinutes();
+    const seconds = currentDate.getSeconds();
+    const date = `${day}.${month}.${year}`;
+
+    $(".date").html(date);
+    setTime(hours, minutes, seconds);
+}
+
+updateClock();
+
+setInterval(updateClock, INTERVAL);
